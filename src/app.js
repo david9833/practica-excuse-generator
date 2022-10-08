e41e1c6a4f9b672713fc72dae053a6796c19b750
david9833 <dagalisteo@gmail.com>
2022-10-08 15:11:18 +0000

    Practica finalizada

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+var title = document.getElementById("the-excuse");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  title.innerHTML = generateExcuse();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var aleatorio1 = Math.floor(Math.random() * who.length);
+  var aleatorio2 = Math.floor(Math.random() * what.length);
+  var aleatorio3 = Math.floor(Math.random() * when.length);
+
+  return who[aleatorio1] + " " + what[aleatorio2] + " " + when[aleatorio3];
 };
